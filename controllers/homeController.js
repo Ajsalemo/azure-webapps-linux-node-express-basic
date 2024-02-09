deed7574db766994674acff07be6878c0ce78d70
Anthony Salemo <ajssalemo@gmail.com>
2024-02-09 18:41:03 -0500

    doing a last test

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res, next) => {
-  res.json({ message: "azure-webapps-linux-node-express-basic" });
+  res.json({ message: "azure-webapps-linux-node-express-basic (MI)" });
 });
 
 export default router;
