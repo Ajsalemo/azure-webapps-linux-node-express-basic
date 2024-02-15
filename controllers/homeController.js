c86daf423abbd6c83c14b5cf1923e6e3f5dbefda
Anthony Salemo <ajssalemo@gmail.com>
2024-02-15 11:03:18 -0500

    testing mi pulls again with aca

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (_req, res, _next) => {
-  res.json({ message: "azure-webapps-linux-node-express-basic (MI2)" });
+  res.json({ message: "azure-webapps-linux-node-express-basic (MI-2)" });
 });
 
 export default router;
